78b89ac8e0f69249c3312393bd50360dbc10fc9f
Subba Codes <subba.codes@gmail.com>
2022-06-12 11:33:37 +0900

    addedreact routes to pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./containers/home";
+import About from "./containers/about";
+import Portfolio from "./containers/portfolio";
+import Skills from "./containers/skills";
+import Resume from "./containers/resume";
+import Contact from "./containers/contact";
 
 function App() {
-  return <h1>JSubba Portfolio</h1>;
+  return (
+    <Routes>
+      <Route index path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 }
 
 export default App;
